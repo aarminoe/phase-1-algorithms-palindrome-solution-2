c5a0d7ff9098dbe588abe3b517a4dd76e4e2bcf1
Aaron Noe <aarminoe@gmail.com>
2022-05-08 08:44:10 -0500

    commit tree cd788d54f010e2b732d2b9b3a409607afae493e1

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const x = word.length - 1 - i
+    if(word[x] !== word[i]) {
+      return false
+    }
+  return true
+}
 }
 
 /* 
   Add your pseudocode here
+  iterated form beginning to middle
+    check each chracter to corresponding letter from the end
+      if any letters don't match, return false
+  
+  return true
 */
 
 /*
   Add written explanation of your solution here
+  make function that returns true if it is a plaindrome. meaning if the word is spelled
+  the same backwards, return true
 */
 
 // You can run `node index.js` to view these console logs
